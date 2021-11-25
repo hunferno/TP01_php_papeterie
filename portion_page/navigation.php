698f1628ec1e1c30e108919bc7a68af9fac2bddf
René KONÉ <kone.rene@hotmail.fr>
2021-11-25 10:06:54 +0100

    TP02-test logique variable php

--- portion_page/navigation.php
+++ portion_page/navigation.php
@@ -6,12 +6,35 @@
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav">
             <!-- Dropdown -->
-            <li class="nav-item dropdown active">
+            <li class="nav-item dropdown <?php if (preg_match('/^prod/', $page)) {
+                                                echo ('active');
+                                            } ?>">
                 <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Les Produits</a>
+
                 <div class="dropdown-menu bg-secondary">
-                    <a class="dropdown-item" href="prod_papeterie.php">Papeterie</a>
-                    <a class="dropdown-item" href="prod_ecriture.php">Ecriture</a>
-                    <a class="dropdown-item" href="#">Accessoirs</a>
+
+                    <!-- <-?php
+                    if ($page == "prod_papeterie") {
+                        # code...
+                        echo ('<a class="dropdown-item" href="prod_ecriture.php">Ecriture</a>');
+                        echo ('<a class="dropdown-item" href="#">Accessoires</a>');
+                    } elseif ($page == "prod_ecriture") {
+                        echo ('<a class="dropdown-item" href="prod_papeterie.php">Papeterie</a>');
+                        echo ('<a class="dropdown-item" href="#">Accessoires</a>');
+                    } else {
+                        echo ('<a class="dropdown-item" href="prod_papeterie.php">Papeterie</a>');
+                        echo ('<a class="dropdown-item" href="prod_ecriture.php">Ecriture</a>');
+                        echo ('<a class="dropdown-item" href="#">Accessoires</a>');
+                    }
+                    ?> -->
+
+                    <a <?php if ($page == 'prod_papeterie') {
+                            echo ('style="display:none"');
+                        } ?> class="dropdown-item" href="prod_papeterie.php">Papeterie</a>
+                    <a <?php if ($page == 'prod_ecriture') {
+                            echo ('style="display:none"');
+                        } ?> class="dropdown-item" href="prod_ecriture.php">Ecriture</a>
+                    <a class="dropdown-item" href="#">Accessoires</a>
                 </div>
             </li>
             <li class="nav-item">
